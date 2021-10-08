00c66382781f38b9c3bdcf1007b3622e22024880
FelipeMel0 <felipedemfernandes@gmail.com>
2021-10-08 11:25:08 -0300

    Testando o projeto

--- produtos/acoes.php
+++ produtos/acoes.php
@@ -23,11 +23,31 @@
 
             move_uploaded_file($_FILES["foto"]["tmp_name"], "fotos/$novoNome");
 
+            /*Recebimento dos dados */
+
+            $descricao = $_POST["descricao"];
+            $peso = $_POST["peso"];
+            $quantidade = $_POST["quantidade"];
+            $cor = $_POST["cor"];
+            $tamanho = $_POST["tamanho"];
+            $valor = $_POST["valor"];
+            $desconto = $_POST["desconto"];
+            $categoriaId = $_POST["categoria"];
+
+            /*Criação da insução sql de inserção */
+
+            $sql = "INSERT INTO tbl_produto (descricao, peso, quantidade, cor, tamanho, valor, desconto, imagem, categoria_id) 
+            VALUES ('$descricao', $peso, $quantidade, '$cor', '$tamanho', $valor, $desconto, '$novoNome', $categoriaId)";
+
+            /*Execução do SQL de inserção */
+
+            $resultado = mysqli_query($conexao, $sql);
+
+            header('location: index.php');
+
             break;
         
         default:
             # code...
             break;
     }
-
-?>
